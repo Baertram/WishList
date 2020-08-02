eccf02fd7635059c85f50c4122c621f8cbb1287f
Baertram <pz1@gmx.net>
2020-08-03 01:07:31 +0200

    -Fixed German typo

--- WishList/lang/de.lua
+++ WishList/lang/de.lua
@@ -196,7 +196,7 @@ local strings = {
     WISHLIST_LAM_FCOIS_MARK_ITEM_AUTO_TT = "Markiert einen gelooteten Set Gegenstand von deiner Wunschliste mit diesem FCO ItemSaver Markierungssymbol",
     WISHLIST_LAM_FCOIS_MARKER_ICONS_PER_CHAR         = "FCOItemSaver - Markierungssymbol je Charakter Wunschliste",
     WISHLIST_LAM_FCOIS_MARK_ITEM_AUTO_PER_CHAR       = "Mark. gelooteten Gegenstand indiv. je Charakter",
-    WISHLIST_LAM_FCOIS_MARK_ITEM_AUTO_PER_CHAR_TT    = "Markiert einen gelooteten Set Gegenstand von der Wunschliste esines deiner Charaktere mit einem FCO ItemSaver Markierungssymbol.\nJeder Charakter kann für seine Wunschliste ein eigenes Symbol nutzen.",
+    WISHLIST_LAM_FCOIS_MARK_ITEM_AUTO_PER_CHAR_TT    = "Markiert einen gelooteten Set Gegenstand von der Wunschliste eines deiner Charaktere mit einem FCO ItemSaver Markierungssymbol.\nJeder Charakter kann für seine Wunschliste ein eigenes Symbol nutzen.",
     WISHLIST_LAM_FCOIS_MARK_ITEM_CHARAKTER_NAME      = "Charakter Name",
     WISHLIST_LAM_FCOIS_MARK_ITEM_ICON                = "Markierungssymbol",
 
